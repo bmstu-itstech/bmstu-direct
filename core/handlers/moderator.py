a556c014cf922ca3a149d37e52b59bef6aaaf6a1
acruxtech <acrux_tech@mail.ru>
2025-02-04 02:37:54 +0300

    fix: small buges

--- core/handlers/moderator.py
+++ core/handlers/moderator.py
@@ -1,8 +1,8 @@
 import logging
 
 from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters import IDFilter, ForwardedMessageFilter, IsReplyFilter, ChatTypeFilter
-from aiogram.types import Message, ChatType, ParseMode
+from aiogram.dispatcher.filters import IDFilter, ForwardedMessageFilter, IsReplyFilter
+from aiogram.types import Message, ParseMode
 
 from core import domain, texts
 
@@ -17,13 +17,13 @@ logger = logging.getLogger(__name__)
 
 
 @dp.message_handler(IDFilter(chat_id=config.comment_chat_id), ForwardedMessageFilter(is_forwarded=True))
-async def handle_ticket_published(message: Message, state: FSMContext, store: Storage):
+async def handle_ticket_published(message: Message, store: Storage):
     ticket_id = extract_ticket_id(message.text)
     await store.update_ticket(ticket_id, group_message_id=message.message_id)
 
 
 @dp.message_handler(IDFilter(chat_id=config.comment_chat_id), IsReplyFilter(is_reply=True))
-async def handle_moderator_answer(message: Message, state: FSMContext, store: Storage):
+async def handle_moderator_answer(message: Message, store: Storage):
     _id = message.__dict__["_values"]["message_thread_id"]
     ticket_id = await store.message_ticket_id(_id)
     await send_moderator_answer(message, store, ticket_id, message.text)
@@ -50,37 +50,6 @@ async def send_moderator_answer(message: Message, store: Storage, ticket_id: int
             owner_message_id=sent.message_id,
             reply_to_message_id=sent.reply_to_message.message_id if sent.reply_to_message else None,
             ticket_id=ticket_id,
-        )
-    )
-
-
-@dp.message_handler(ChatTypeFilter(ChatType.PRIVATE), state="*")
-async def handle_student_answer(message: Message, state: FSMContext, store: Storage):
-    if not message.reply_to_message:
-        await message.answer(texts.errors.no_reply)
-        return
-    ticket_ids = await store.chat_ticket_ids(message.chat.id)
-    replied_message = await store.message_by_id(
-        ticket_ids=ticket_ids,
-        owner_message_id=message.reply_to_message.message_id,
-    )
-    await send_student_answer(message, store, replied_message, message.text)
-
-
-async def send_student_answer(message: Message, store: Storage, replied_message: domain.Message, answer: str):
-    sent = await bot.send_message(
-        config.comment_chat_id,
-        texts.ticket.student_answer(answer),
-        reply_to_message_id=replied_message.message_id,
-        parse_mode=ParseMode.HTML,
-    )
-    await store.save_message(
-        domain.Message(
-            chat_id=sent.chat.id,
-            message_id=sent.message_id,
-            owner_message_id=message.message_id,
-            reply_to_message_id=sent.reply_to_message.message_id,
-            ticket_id=replied_message.ticket_id,
         )
     )
 
--- core/handlers/student.py
+++ core/handlers/student.py
@@ -2,7 +2,8 @@ import logging
 import re
 
 from aiogram.dispatcher import FSMContext
-from aiogram.types import Message, ReplyKeyboardRemove, ParseMode
+from aiogram.dispatcher.filters import ChatTypeFilter, IsReplyFilter
+from aiogram.types import Message, ReplyKeyboardRemove, ParseMode, ChatType
 
 from core import texts
 from core import states
@@ -28,7 +29,7 @@ DATA_TEXT_KEY = "text"
 logger = logging.getLogger(__name__)
 
 
-@dp.message_handler(state="*", commands=["start"])
+@dp.message_handler(ChatTypeFilter(ChatType.PRIVATE), state="*", commands=["start"])
 async def send_start(message: Message, state: FSMContext):
     await state.finish()
     await message.answer(
@@ -48,7 +49,35 @@ async def send_create_ticket(message: Message):
     await states.Registration.create_ticket.set()
 
 
-@dp.message_handler(state=states.Registration.create_ticket, regexp=texts.buttons.create_ticket)
+@dp.message_handler(ChatTypeFilter(ChatType.PRIVATE), IsReplyFilter(is_reply=True), state="*")
+async def handle_student_answer(message: Message, store: Storage):
+    ticket_ids = await store.chat_ticket_ids(message.chat.id)
+    replied_message = await store.message_by_id(
+        ticket_ids=ticket_ids,
+        owner_message_id=message.reply_to_message.message_id,
+    )
+    await send_student_answer(message, store, replied_message, message.text)
+
+
+async def send_student_answer(message: Message, store: Storage, replied_message: domain.Message, answer: str):
+    sent = await bot.send_message(
+        config.comment_chat_id,
+        texts.ticket.student_answer(answer),
+        reply_to_message_id=replied_message.message_id,
+        parse_mode=ParseMode.HTML,
+    )
+    await store.save_message(
+        domain.Message(
+            chat_id=sent.chat.id,
+            message_id=sent.message_id,
+            owner_message_id=message.message_id,
+            reply_to_message_id=sent.reply_to_message.message_id,
+            ticket_id=replied_message.ticket_id,
+        )
+    )
+
+
+@dp.message_handler(ChatTypeFilter(ChatType.PRIVATE), state=states.Registration.create_ticket, regexp=texts.buttons.create_ticket)
 async def handle_create_ticket(message: Message):
     await send_choice_issue(message)
 
@@ -62,14 +91,21 @@ async def send_choice_issue(message: Message):
     await states.Registration.choice_issue.set()
 
 
-@dp.message_handler(state=states.Registration.choice_issue)
+@dp.message_handler(ChatTypeFilter(ChatType.PRIVATE), state=states.Registration.choice_issue)
 async def handle_choice_issue(message: Message, state: FSMContext):
+    if message.text == texts.buttons.back:
+        return await send_cancel_ticket(message, state)
     issue = map_button_to_issue(message.text)
     if not issue:
         return await send_choice_issue_invalid(message)
     async with state.proxy() as data:
         data[DATA_ISSUE_KEY] = issue
     await send_choice_category(message)
+
+
+async def send_cancel_ticket(message: Message, state: FSMContext):
+    await message.answer(texts.ticket.cancel_ticket, reply_markup=keyboards.create_ticket_keyboard())
+    await states.Registration.create_ticket.set()
 
 
 async def send_choice_issue_invalid(message: Message):
@@ -88,13 +124,17 @@ async def send_choice_category(message: Message):
     await states.Registration.choice_category.set()
 
 
-@dp.message_handler(state=states.Registration.choice_category)
+@dp.message_handler(ChatTypeFilter(ChatType.PRIVATE), state=states.Registration.choice_category)
 async def handle_choice_category(message: Message, state: FSMContext):
     if message.text == texts.buttons.back:
         return await send_choice_issue(message)
     category = map_button_to_category(message.text)
     if not category:
         return await send_choice_category_invalid(message)
+    if category is domain.Category.MILITARY:
+        return await send_choice_category_military(message, state)
+    if category is domain.Category.ADMISSION:
+        return await send_choice_category_admission(message, state)
     async with state.proxy() as data:
         data[DATA_CATEGORY_KEY] = category
     await send_choice_privacy(message)
@@ -103,6 +143,16 @@ async def handle_choice_category(message: Message, state: FSMContext):
 async def send_choice_category_invalid(message: Message):
     await message.answer(texts.errors.invalid_input_button)
     await send_choice_category(message)
+
+
+async def send_choice_category_military(message: Message, state: FSMContext):
+    await message.answer(texts.ticket.chosen_military, reply_markup=ReplyKeyboardRemove())
+    await state.finish()
+
+
+async def send_choice_category_admission(message: Message, state: FSMContext):
+    await message.answer(texts.ticket.chosen_admission, reply_markup=ReplyKeyboardRemove())
+    await state.finish()
 
 
 async def send_choice_privacy(message: Message):
@@ -114,7 +164,7 @@ async def send_choice_privacy(message: Message):
     await states.Registration.choice_privacy.set()
 
 
-@dp.message_handler(state=states.Registration.choice_privacy)
+@dp.message_handler(ChatTypeFilter(ChatType.PRIVATE), state=states.Registration.choice_privacy)
 async def handle_choice_privacy(message: Message, state: FSMContext):
     if message.text == texts.buttons.back:
         return await send_choice_category(message)
@@ -146,9 +196,13 @@ async def send_input_full_name(message: Message):
     await states.Registration.input_full_name.set()
 
 
-@dp.message_handler(state=states.Registration.input_full_name)
+@dp.message_handler(ChatTypeFilter(ChatType.PRIVATE), state=states.Registration.input_full_name)
 async def handle_input_full_name(message: Message, state: FSMContext):
-    if not re.match(r"\w+ \w+", message.text):
+    words = message.text.split()
+    if not (
+        2 <= len(words) <= 3 and
+        all(re.match(r"^[А-ЯЁ][а-яё\-]+$", word) for word in words)
+    ):
         return await send_input_full_name_invalid(message)
     full_name = message.text
     async with state.proxy() as data:
@@ -170,9 +224,9 @@ async def send_input_study_group(message: Message):
     await states.Registration.input_study_group.set()
 
 
-@dp.message_handler(state=states.Registration.input_study_group)
+@dp.message_handler(ChatTypeFilter(ChatType.PRIVATE), state=states.Registration.input_study_group)
 async def handle_input_study_group(message: Message, state: FSMContext):
-    if not re.match(r"[А-Я]{1,3}1?[0-9]-[1-9]{2,3}[АБМ]?", message.text):
+    if not validate_group(message.text):
         return await send_input_study_group_invalid(message)
     study_group = message.text
     async with state.proxy() as data:
@@ -194,12 +248,18 @@ async def send_input_text(message: Message):
     await states.Registration.input_text.set()
 
 
-@dp.message_handler(state=states.Registration.input_text)
+@dp.message_handler(ChatTypeFilter(ChatType.PRIVATE), state=states.Registration.input_text)
 async def handle_input_text(message: Message, state: FSMContext):
     text = message.text
+    if not validate_text(text):
+        return await send_bad_text(message)
     async with state.proxy() as data:
         data[DATA_TEXT_KEY] = text
     await send_choice_approve(message)
+
+
+async def send_bad_text(message: Message):
+    await message.answer(texts.ticket.bad_text)
 
 
 async def send_choice_approve(message: Message):
@@ -211,7 +271,7 @@ async def send_choice_approve(message: Message):
     await states.Registration.choice_approve.set()
 
 
-@dp.message_handler(state=states.Registration.choice_approve)
+@dp.message_handler(ChatTypeFilter(ChatType.PRIVATE), state=states.Registration.choice_approve)
 async def handle_choice_approve(message: Message, state: FSMContext, store: Storage):
     if message.text == texts.buttons.yes:
         return await save_ticket(message, state, store)
@@ -265,6 +325,13 @@ async def send_ticket_was_sent(message: Message, ticket_id: int):
     await send_create_ticket(message)
 
 
+@dp.message_handler(ChatTypeFilter(ChatType.PRIVATE), state="*")
+async def unknown(message: Message, state: FSMContext, store: Storage):
+    if not message.reply_to_message:
+        return await message.answer(texts.errors.no_reply)
+    await message.answer(texts.errors.unknown)
+
+
 def map_button_to_issue(btn: str) -> domain.Issue | None:
     match btn:
         case texts.buttons.question:
@@ -299,3 +366,54 @@ def map_button_to_category(btn: str) -> domain.Category | None:
         case texts.buttons.other:
             return domain.Category.OTHER
     return None
+
+
+valid_faculties = {
+  "Москва": ["АК", "БМТ", "ВИ", "ИБМ", "ИСОТ", "ИУ", "Л", "МТ", "ОЭ", "ПС", "РК", "РКТ", "РЛ", "РТ", "СГН", "СМ", "Т", "ФН", "ФО", "ЦДО", "Э", "ЮР"],
+  "Калуга": ["ВУЦ-КФ", "ИУК", "МК", "ПОДК"],
+  "Мытищи": ["К", "ЛТ"]
+}
+
+degree_semester = {
+  'Б': [1, 8],
+  'М': [1, 4],
+  'А': [1, 10],
+  'С': [1, 12]
+}
+
+
+def validate_group(group_name: str) -> bool:
+    regex = re.compile(
+        r"^(?P<faculty>[А-Я]+)(?P<department>\d{1,2})(?P<modifier>[ИЦ]?)?-(?P<semester>\d{1,2})(?P<group_number>\d)(?P<degree>[БМА]?)$")
+    match = regex.match(group_name)
+    if not match:
+        return False
+
+    faculty = match.group("faculty")
+    department = int(match.group("department"))
+    modifier = match.group("modifier") or ""
+    semester = int(match.group("semester"))
+    group_number = int(match.group("group_number"))
+    degree = match.group("degree") or "С"
+
+    if modifier not in ["", "Ц", "И"]:
+        return False
+
+    faculty_found = False
+    for _branch, faculties in valid_faculties.items():
+        if faculty in faculties:
+            faculty_found = True
+            break
+
+    left_border = degree_semester[degree][0]
+    right_border = degree_semester[degree][1]
+    return faculty_found \
+        and (1 <= department <= 13) \
+        and (left_border <= semester <= right_border) \
+        and (1 <= group_number <= 9)
+
+
+def validate_text(text: str) -> bool:
+    with open('assets/bad_words.txt', 'r', encoding='utf-8') as file:
+        bad_words = {line.strip() for line in file}
+    return all(word not in text for word in bad_words)
